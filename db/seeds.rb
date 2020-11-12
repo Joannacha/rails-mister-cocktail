76b4aa830cba4e82f1872e5ed978200319ec28ba
Joanna Chaillou <joanna.chaillou@edu.escp.eu>
2020-11-12 12:02:36 +0100

    seed with 100 ingredients JSON

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require "json"
+require "open-uri"
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Deleting all records"
+Ingredient.delete_all
+
+puts "Seeding all ingredients from JSON file"
+response = open("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list").read
+data = JSON.parse(response)
+
+data["drinks"].each do |item|
+  ingredient = Ingredient.new(
+    name: item["strIngredient1"]
+  )
+  ingredient.save!
+end
+
+puts "All done :)"
+
